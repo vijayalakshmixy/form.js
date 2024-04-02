a688bafa6d444f3799395971c0cc25059b3d5f3d
vijayalakshmixy <159040500+vijayalakshmixy@users.noreply.github.com>
2024-04-02 16:57:37 +0530

    Add files via upload

---
+++ form1.js
@@ -0,0 +1,62 @@
+
+// let btn=document.getElementById('btn');
+// btn.addEventListener("click",function(event){
+//     event.preventDefault();
+//     console.log(Input.value)
+// });
+
+
+
+
+let a=document.getElementById("firstname");
+a.addEventListener("input",()=>{
+    let a1=a.value 
+    console.log(a1);
+});
+
+let b=document.getElementById("lastname");
+b.addEventListener("input",()=>{
+    let b1=b.value 
+    console.log(b1);
+});
+
+let c=document.getElementById("managername");
+c.addEventListener("input",()=>{
+    let c1=c.value 
+    console.log(c1);
+})
+
+let d=document.getElementById("managername1");
+d.addEventListener("input",()=>{
+    let d1=d.value 
+    console.log(d1);
+})
+
+let e=document.getElementById("task");
+e.addEventListener("input",()=>{
+    let e1=e.value 
+    console.log(e1);
+})
+
+let f=document.getElementById("taskid");
+f.addEventListener("input",()=>{
+    let f1=f.value 
+    console.log(f1);
+})
+
+let g=document.getElementById("adress");
+g.addEventListener("input",()=>{
+    let g1=g.value 
+    console.log(g1);
+})
+
+let h=document.getElementById("instructions");
+h.addEventListener("input",()=>{
+    let h1=h.value 
+    console.log(h1);
+})
+
+
+
+
+
